a4206a21b96a7108a7620caa5a1a05747085beea
VitorVitalicy <vitorsantos.vaas@gmail.com>
2023-03-27 01:01:57 -0300

    atualização 27 de março 2023 madrugada

--- JSfiles/file.js
+++ JSfiles/file.js
@@ -1,25 +1,60 @@
-const studiesDataTotal = {row1: 'teste'}
+const studiesDataTotal = {row1: '',
+                        row2:'',
+                        row3:'', 
+                        row4:'', 
+                        row5:'', 
+                        row6:'', 
+                        row7:'', 
+                        row8:'', }
+
+        var count = 0;
 
         function addStudy(){
+            document.getElementById("data").innerHTML ='';
+
             const studiesData = [];
+            let row = '';
 
             studiesData.push(document.getElementById("iname").value);
             studiesData.push(document.getElementById("iplace").value);
             studiesData.push(document.getElementById("iserie").value);
             studiesData.push(document.getElementById("ivalue").value);
 
-            studiesDataTotal.row2 = studiesData;
+            switch(count){
+                case 1:
+                    studiesDataTotal.row1 = studiesData;
+                    break;
+                case 2:
+                    studiesDataTotal.row2 = studiesData;
+                    break;
+                case 3:
+                    studiesDataTotal.row4 = studiesData;
+                    break;
+                case 4:
+                    studiesDataTotal.row4 = studiesData;
+                    break;
+                case 5:
+                    studiesDataTotal.row5 = studiesData;
+                    break;
+                case 6:
+                    studiesDataTotal.row6 = studiesData;
+                    break;
+                case 7:
+                    studiesDataTotal.row7 = studiesData;
+                    break;
+            }
 
-            document.getElementById("00").innerHTML = studiesDataTotal.row2[0];
-            document.getElementById("01").innerHTML = studiesDataTotal.row2[1];
-            document.getElementById("02").innerHTML = studiesDataTotal.row2[2];
-            document.getElementById("03").innerHTML = studiesDataTotal.row2[3];
+            for(let key in studiesDataTotal){
+                if(studiesDataTotal[key]!=''){
+                    for(let j=0; j<=4; j++){
+                        let line = studiesDataTotal[key]
+                        row+= "<td>" + line[j] +  "</td>";
+                    }
+                    document.getElementById("data").innerHTML += "<tr>" + row  + "</tr>";
+                    row = '';
+                }
+            }
 
-
-            document.getElementById("iname").value ='';
-            document.getElementById("iplace").value ='';
-            document.getElementById("iserie").value ='';
-            document.getElementById("ivalue").value ='';
+            count +=1;
+            
         }
-
-
